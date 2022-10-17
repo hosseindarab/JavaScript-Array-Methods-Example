204eb4c75d78b2ccdd7d1056b21d196d35bdf7c1
hosseindarab <hdarab400@gmail.com>
2022-10-18 00:35:14 +0200

    - slice method

---
+++ slice.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3, 4, 5, 6]
+
+const numbers2 = numbers.slice(1)
+
+console.log(numbers2)
+
+
+
+const participants = ['Florin', 'Ivan', 'Liam', 'Jai', 'Patrik']
+
+// slice(start_index, end_index + 1)
+const winners = participants.slice(0, 3)
+
+console.log(winners)
