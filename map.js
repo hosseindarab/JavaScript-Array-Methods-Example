b34658c1b321002705f8441ee96dee2cd57eb7dc
hosseindarab <hdarab400@gmail.com>
2022-11-21 13:16:09 +0100

    - await in for loop and map

--- map.js
+++ map.js
@@ -53,3 +53,50 @@ const strArr = ['1', '2', '3', '4']
 const numbersArr = strArr.map(item => Number(item))
 
 console.log(numbersArr)
+
+
+
+
+
+const fruitBasket = {
+    apple: 27,
+    orange: 0,
+    banana: 14
+  }
+
+const fruitsToGet = ['apple', 'orange', 'banana']
+
+const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+  
+  const getNumFruit = fruit => {
+    return sleep(1000).then(v => fruitBasket[fruit])
+  }
+
+  const forLoop = async _ => {
+    console.log('Start')
+  
+    for (let index = 0; index < fruitsToGet.length; index++) {
+      const fruit = fruitsToGet[index]
+      const numFruit = await getNumFruit(fruit)
+      console.log(numFruit)
+    }
+  
+    console.log('End')
+  }
+  forLoop()
+
+// const mapLoop = async _ => {
+//     console.log('Start')
+
+//     const promises = fruitsToGet.map(async fruit => {
+//         const numFruit = await getNumFruit(fruit)
+//         return numFruit
+//     })
+
+//     const numFruits = await Promise.all(promises)
+//     console.log(numFruits)
+
+//     console.log('End')
+// }
